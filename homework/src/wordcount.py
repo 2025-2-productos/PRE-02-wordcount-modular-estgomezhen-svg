dafb2d3de74136c40b240781b6ff4d1de7917698
estgomezhen-svg <estgomezhen@unal.edu.co>
2025-11-15 12:10:35 -0500

    ADD:Progreso codigo

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -2,15 +2,15 @@
 import os
 
 
-def read_all_lines():
-    all_lines = []
-    input_files_list = os.listdir("data/input/")
+# def read_all_lines():
+#     all_lines = []
+#     input_files_list = os.listdir("data/input/")
 
-    for filename in input_files_list:
-        with open(filename, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-            all_lines.extend(lines)
-    return all_lines
+#     for filename in input_files_list:
+#         with open(filename, "r", encoding="utf-8") as f:
+#             lines = f.readlines()
+#             all_lines.extend(lines)
+#     return all_lines
 
 
 def main():
